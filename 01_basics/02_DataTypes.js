e8d078732f51868626dbb4be57f1f4381f697d35
Krishna Prasad Swain <krishna.prasad.swain2405@gmail.com>
2024-12-13 04:27:28 +0000

    JS-DataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 * 3)  // we are using node.js not browser
+
+console.log(3 + 3)
+
+console.log("Krishna")
+
+let name = "Krishna"
+let age = 17
+let isLoggedIn = false
+
+//number => 
+//bigint =>
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null) //output: object
+console.log(typeof undefined) //output: undefined
